d4d5ed0de4aea9b9ab95204ee47777856d8a59cf
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-04-08 12:42:23 +0630

    About done

---
+++ resources/views/components/home/hero.blade.php
@@ -0,0 +1,39 @@
+<!-- ====== Portfolio Section Start -->
+<section
+    x-data="
+        {
+          showCards: 'all',
+          activeClasses: 'bg-primary text-white',
+          inactiveClasses: 'text-body-color hover:bg-primary hover:text-white',
+        }
+      "
+    class="pt-20 lg:py-[80px] pb-12 bg-indigo-700 text-gray-100 dark:bg-indigo-800"
+>
+    <div class="container">
+        <div class="flex flex-wrap -mx-4">
+            <div class="w-full px-4">
+                <div class="text-center mx-auto mb-[60px] max-w-[510px]">
+              <span class="font-semibold text-lg text-primary mb-2 block">
+                Our Portfolio
+              </span>
+                    <h2
+                        class="
+                  font-bold
+                  text-3xl
+                  sm:text-4xl
+                  md:text-[40px]
+                  text-dark
+                  mb-4
+                "
+                    >
+                        Our Recent Projects
+                    </h2>
+                    <p class="text-base text-body-color">
+                        There are many variations of passages of Lorem Ipsum available
+                        but the majority have suffered alteration in some form.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,4 @@
 <x-app-layout>
-    <div class="p-20 w-full text-center bg-white  border shadow-md sm:p-8 dark:bg-gray-800
-    dark:border-gray-700">
-        <h5 class="mb-2 text-3xl font-bold text-gray-900 dark:text-white">Work fast from anywhere</h5>
-        <p class="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">Stay up to date and move work forward with
-            Flowbite on iOS & Android. Download the app today.</p>
-        <div class="justify-center items-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
-            <a href="#portofolio"
-               class="w-full sm:w-auto bg-gray-800 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
-                <div class="text-left">
-                    <div class="-mt-1 font-sans text-sm font-semibold">My Projects</div>
-                </div>
-            </a>
-        </div>
-    </div>
+    <x-home.hero></x-home.hero>
+    <x-home.about></x-home.about>
 </x-app-layout>
